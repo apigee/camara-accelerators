2943398b77e813068fd885b75b23288f30db6219
Carlos Cabral <carloscabral@google.com>
2025-03-08 14:40:14 -0300

    fix: multiple fixes for first release

--- camara-sim-swap/apiproxy/resources/jsc/verify-scope-1.0.js
+++ camara-sim-swap/apiproxy/resources/jsc/verify-scope-1.0.js
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * This code snippet validates the presence of an expected scope within a set of consented scopes.
  * It retrieves the consented and expected scopes from context variables, checks if the expected scope
  * is included within the consented scopes, and sets error variables if the expected scope is not found.
@@ -7,8 +23,11 @@
 // Retrieve the consented scopes from the context variable.
 var consentedScopes = context.getVariable("scope");
 
+// Retrieve the expected scope from the context variable. This scope is defined in a property set named "check".
+var expectedOperationScope = context.getVariable("flow.expectedOperationScope");
+
 // Retrieve the expected scope from the context variable. This scope is defined in a property set named "simswap".
-var expectedScope = context.getVariable("propertyset.simswap.scope");
+var expectedGlobalScope = context.getVariable("flow.expectedGlobalScope");
 
 /**
  * Splits the consented scopes string into an array of individual scopes.
@@ -32,10 +51,11 @@ consentedScopes = splitScopes(consentedScopes);
  * Sets error variables in the context if the expected scope is not found.
  *
  * @param {string[]} consentedScopes - An array of consented scopes.
- * @param {string} expectedScope - The expected scope to validate.
+ * @param {string} expectedOperationScope - The expected scope to validate.
+ * @param {string} expectedGlobalScope - The expected scope to validate.
  */
-function validateScope(consentedScopes, expectedScope) {
-  if (!consentedScopes.includes(expectedScope)) {
+function validateScope(consentedScopes, expectedOperationScope, expectedGlobalScope) {
+  if (!consentedScopes.includes(expectedOperationScope) && !consentedScopes.includes(expectedGlobalScope)) {
     // Set the error type to "unauthorized".
     context.setVariable("error_type", "unauthorized");
     // Set a descriptive error message.
@@ -46,4 +66,4 @@ function validateScope(consentedScopes, expectedScope) {
 }
 
 // Validate the presence of the expected scope.
-validateScope(consentedScopes, expectedScope);
+validateScope(consentedScopes, expectedOperationScope, expectedGlobalScope);
